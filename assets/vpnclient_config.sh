ac6f77ec5ab635f85fe3db141b603480aea784bf
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2018-09-18 22:51:39 +0200

    Fixed errors on container restart

--- assets/vpnclient_config.sh
+++ assets/vpnclient_config.sh
@@ -8,8 +8,15 @@ do
   while read line
   do
     printf "$line" 1>&2
-  
-    vpncmd localhost /CLIENT /CMD NicCreate ${SE_NICNAME} 1>&2
+    
+    rm -f /usr/vpnclient/vpn_client.config
+    
+    if grep -q vpn_${SE_NICNAME} /proc/net/dev; then 
+      printf "NIC ${SE_NICNAME} already exists, skipping creation" 1>&2
+    else
+      vpncmd localhost /CLIENT /CMD NicCreate ${SE_NICNAME} 1>&2
+    fi
+      
     vpncmd localhost /CLIENT /CMD AccountCreate ${SE_ACCOUNT_NAME} /SERVER:${SE_SERVER} /HUB:${SE_HUB} /USERNAME:${SE_USERNAME} /NICNAME:${SE_NICNAME} 1>&2
     vpncmd localhost /CLIENT /CMD AccountPasswordSet ${SE_ACCOUNT_NAME} /PASSWORD:${SE_PASSWORD} /TYPE:${SE_TYPE} 1>&2
     vpncmd localhost /CLIENT /CMD AccountConnect ${SE_ACCOUNT_NAME} 1>&2
